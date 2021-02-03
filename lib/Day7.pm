ff2ae094b8e479b9dc74998e841dce54180ee1d8
keutoi <k3tu0isui@gmail.com>
2021-02-03 11:32:29 +0530

    day7: perl; good network representation and traversal

--- lib/Day7.pm
+++ lib/Day7.pm
@@ -4,7 +4,7 @@ package Day7;
 
 use strict;
 use warnings;
-use List::Util qw(all reduce);
+use List::Util qw(all reduce uniqint);
 
 sub parse_disc{
     my ($line) = @_;
@@ -47,7 +47,6 @@ sub create_network {
     for my $node (keys %network) {
 	if (!defined $network{$node}->{children}) {
 	    $network{$node}->{mark} = 1;
-	    $network{$node}->{cumulative_weight} = $network{$node}->{weight};
 	};
     }
     my @nodes = keys %network;
@@ -63,7 +62,7 @@ sub create_network {
 		    $network{$parent}->{mark} = 1;
 		    $network{$parent}->{cumulative_weight} +=
 		      reduce {$a + $b}
-		      (map {$network{$_}->{weight}} keys %{$network{$parent}->{children}})
+		      (map {$network{$_}->{cumulative_weight}} keys %{$network{$parent}->{children}})
 		};
 	    }
 	}
@@ -75,14 +74,17 @@ sub create_network {
 	push @roots, $node if @parents == 0;
     }
 
-    return [\%network, \@roots];
+    return (\%network, \@roots);
 }
 
 sub write_network_graphviz {
-    my ($network, $outfile) = @_;
+    my ($network, $outfile, $highlight_nodes) = @_;
     open my $fh, '>', $outfile or die "File Error $outfile: $!";
     print $fh "digraph day7network {\n";
     print $fh "node [shape=point]\nedge [arrowhead=none arrowtail=none]\n";
+    for (@$highlight_nodes) {
+	print $fh "$_ [color=red]\n";
+    }
     my @nodes = keys %$network;
     for my $node (@nodes) {
 	my @children = keys %{$network->{$node}{children}};
@@ -94,10 +96,39 @@ sub write_network_graphviz {
     close $fh;
 }
 
-sub find_error_node {
+sub find_error_node { # error node, and weight to be decreased
     my ($network, $root) = @_;
     my $current_node = $root;
-    # until () {
-    # }
+    my $sibling_weight = undef;
+    while (1) {
+	my @children = keys %{$network->{$current_node}{children}};
+	my @weights = uniqint map {$network->{$_}{cumulative_weight}} @children;
+	return ($current_node,
+		$network->{$current_node}{cumulative_weight} - $sibling_weight)
+	  if @weights == 1;
+	warn "Node: $current_node has @weights different weights"
+	  and return if @weights > 2;
+	my ($w1, $w2) = @weights;
+	my (@w1_nodes, @w2_nodes);
+	for my $child (@children) {
+	    if ($network->{$child}{cumulative_weight} == $w1) {
+		push @w1_nodes, $child;
+	    } else {
+		push @w2_nodes, $child
+	    }
+	}
+	if (@w1_nodes == 1) {
+	    ($current_node) = @w1_nodes;
+	    $sibling_weight = $w2;
+	} elsif (@w2_nodes == 1) {
+	    ($current_node) = @w2_nodes;
+	    $sibling_weight = $w1;
+	} else {
+	    warn <<EMSG and return;
+"Could not find distinguish b/w nodes.
+They are  partitioned as ${\ (scalar @w1_nodes) } ${\(scalar @w2_nodes)}"
+EMSG
+	}
+    }
 }
 1;
--- t/day7.t
+++ t/day7.t
@@ -21,30 +21,7 @@ ugml (68) -> gyxo, ebii, jptl
 gyxo (61)
 cntj (57)
 EOF
-
-my $ex_parsed = {
-		 tknk => [41, {
-			       ugml => [68, {
-					     gyxo => [61],
-					     ebii => [61],
-					     jptl => [61],
-					    }
-				       ],
-			       padx => [45, {
-					     pbga => [66],
-					     havc => [66],
-					     qoya => [66],
-					    }
-				       ],
-			       fwft => [72, {
-					     ktlj => [57],
-					     cntj => [57],
-					     xhth => [57],
-					    }
-				       ]
-			      }
-			 ]
-		};
+;
 
 is_deeply(Day7::parse_disc("pbga (66)"), ["pbga", 66], "parse test pbga");
 is_deeply(Day7::parse_disc("fwft (72) -> ktlj, cntj, xhth"),
@@ -53,12 +30,15 @@ is_deeply(Day7::parse_disc("fwft (72) -> ktlj, cntj, xhth"),
 my @lines = split(/\n/, $ex);
 is(Day7::find_root(\@lines), 'tknk', 'root node using find_root');
 
-my $network_root = Day7::create_network(\@lines);
-print Dumper(%{$network_root->[0]}{$network_root->[1]->[0]});
-my @roots = @{$network_root->[1]};
-is(scalar @roots, 1, 'number of root nodes');
-is($roots[0], 'tknk', 'root node using create_network');
-# graphviz visual
+my ($network, $roots) = Day7::create_network(\@lines);
+print Dumper($network);
+is(scalar @$roots, 1, 'number of root nodes');
+is($roots->[0], 'tknk', 'root node using create_network');
+# for graphviz visuals
 # Day7::write_network_graphviz($network_roots->[0], '/tmp/day7graph');
+my ($error_node, $error_weight) = Day7::find_error_node($network,
+							$roots->[0]);
+is($error_node, 'ugml', "error node name");
+is($error_weight, 8, "error node weight correction");
 
 done_testing();
